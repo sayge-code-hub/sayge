cf6aa403235d9842d74b365e08319bde51c20d98
Aditya Rana <adityaarana@outlook.com>
2025-02-05 00:12:26 +0530

    Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+//   base: '/sayge/',
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  base: '/sayge/',
+  base: '/sayge/', // 👈 Must match your GitHub repo name
 });
