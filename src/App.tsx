43c5370638fed3a0741b64384be2a373d80414a8
Aditya Rana <adityaarana@outlook.com>
2025-02-07 18:27:45 +0530

    Fix navigation issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/sayge">
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
