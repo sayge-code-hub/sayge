0675a473f5ef70ea00a3ce18c6581273a5e5c45e
Aditya Rana <adityaarana@outlook.com>
2025-02-13 19:44:51 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sayge/sw.js', { scope: '/sayge/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
